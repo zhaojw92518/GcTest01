f644fa95309392575f147a7c8cbcbf56f4d6ec02
zhaojw92518 <516090359@qq.com>
2014-11-27 13:58:22 +0800

    Init

---
+++ GcTest01/TestClass.h
@@ -0,0 +1,14 @@
+#ifndef TEST_CLASS_H
+#define TEST_CLASS_H
+
+class TestClass{
+private:
+	int id;
+	double d_id;
+public:
+	int g_id;
+	TestClass();
+	void print_ids();
+};
+
+#endif
---
+++ GcTest01/u_ptr.h
@@ -0,0 +1,10 @@
+#ifndef U_PTR_H
+#define U_PTR_H
+
+template <class T>
+class u_ptr{
+	typename T* data_ptr;
+
+};
+
+#endif
